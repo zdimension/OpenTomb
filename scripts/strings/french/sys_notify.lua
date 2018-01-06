4bf04734366066182d43bb3480187b9eb4f70891
Monsieur Z <zippedfire@free.fr>
2018-01-06 16:41:16 +0100

    Finished French sys_notify.lua

--- scripts/strings/french/sys_notify.lua
+++ scripts/strings/french/sys_notify.lua
@@ -1,7 +1,7 @@
 -- OPENTOMB SYSTEM CONSOLE NOTIFICATIONS
 -- by Lwmte, Apr 2015
 -- French Translation by: zdimension.
--- Translation Version: 1.0.0.0 (December 18th, 2016)
+-- Translation Version: 1.1.0.0 (January 6th, 2018)
 
 --------------------------------------------------------------------------------
 -- Here you have some global system console warnings which will frequently show.
@@ -20,35 +20,35 @@ sys_notify[009]     = "Impossible de créer la police. Peut-être maximum attein
 sys_notify[010]     = "Impossible de créer le style. Peut-être maximum atteint ? (%d / %d)";
 sys_notify[011]     = "La police avec l'ID spécifié n'existe pas ou n'a pu être supprimée !";
 sys_notify[012]     = "Le style avec l'ID spécifié n'existe pas ou n'a pu être supprimé !";
-sys_notify[013]     = "Can't find skeletal model with id #%d!";
+sys_notify[013]     = "Impossible de trouver le squelette avec l'ID #%d!";
 sys_notify[014]     = "Numéro d'animation incorrect : #%d !";
-sys_notify[015]     = "Wrong animation dispatch number: #%d !";
+sys_notify[015]     = "Numéro de dispatch d'animation incorrect : #%d !";
 sys_notify[016]     = "Numéro d'image incorrect : #%d !";
 sys_notify[017]     = "ID de flux incorrect. Doit être supérieur ou égal à 0.";
 sys_notify[018]     = "ID de son incorrect. Doit être dans l'intervalle 0..%d.";
 sys_notify[019]     = "Erreur Audio_Send : aucun canal libre.";
-sys_notify[020]     = "Erreur Audio_Send : no such sample.";
-sys_notify[021]     = "Audio_Send: sample skipped - please retry !";
-sys_notify[022]     = "Audio_Kill: sample %d isn't playing.";
-sys_notify[023]     = "Flipmap bit %d state is FALSE - no rooms were swapped !";
+sys_notify[020]     = "Erreur Audio_Send : échantillon non trouvé.";
+sys_notify[021]     = "Audio_Send: échantillon ignoré - réessayez !";
+sys_notify[022]     = "Audio_Kill: l'échantillon %d n'est pas actif.";
+sys_notify[023]     = "Bit de flipmap %d inactif - aucune pièce échangée !";
 sys_notify[024]     = "Fichier introuvable - \"%s\"";
 sys_notify[025]     = "Avertissement : l'image %s n'est pas truecolor - non supportée !";
 sys_notify[026]     = "SDL n'a pu charger %s: %s";
 sys_notify[027]     = "Décalage d'image incorrect";
 sys_notify[028]     = "Erreur : impossible d'ouvrir le fichier !";
 sys_notify[029]     = "Erreur : mauvais format de fichier !";
-sys_notify[030]     = "Invalid console line count value !";
-sys_notify[031]     = "Flipmap with index %d doesn't exist!";
-sys_notify[032]     = "Hair setup with index %d doesn't exist!";
+sys_notify[030]     = "Nombre de lignes de console invalide !";
+sys_notify[031]     = "Impossible de trouver la flipmap avec l'index %d !";
+sys_notify[032]     = "Impossible de trouver la configuration de chevelure avec l'index %d !";
 sys_notify[033]     = "Impossible de créer une chevelure pour le personnage %d!";
-sys_notify[034]     = "No hairs for character %d - nothing to remove.";
-sys_notify[035]     = "Ragdoll setup with index %d is corrupted or doesn't exist!";
-sys_notify[036]     = "Can't create ragdoll for entity %d !";
-sys_notify[037]     = "Can't remove ragdoll for entity %d - possibly no ragdoll?";
+sys_notify[034]     = "Pas de chevelure pour le personnage %d - rien à supprimer.";
+sys_notify[035]     = "La configuration de ragdoll avec l'index %d est corrompue ou inexistante !";
+sys_notify[036]     = "Impossible de créer un ragdoll pour l'entité %d !";
+sys_notify[037]     = "Impossible de supprimer le ragdoll pour l'entité %d - peut-être pas de ragdoll ?";
 
-sys_notify[1000]    = "Ogg stream opened (%s): channels = %d, sample rate = %d, bitrate = %.1f";
+sys_notify[1000]    = "Flux Ogg ouvert (%s) : canaux = %d, fréq. d'éch. = %d, bitrate = %.1f";
 sys_notify[1001]    = "Lecture du fichier: \"%s\"";
-sys_notify[1002]    = "Giving item %i x%i to entity %x";
+sys_notify[1002]    = "Don de l'élément %i x%i à l'entité %x";
 sys_notify[1003]    = "Changement de l'ID du niveau actuel vers %d";
 sys_notify[1004]    = "Changement du jeu actuel vers %d";
 sys_notify[1005]    = "Version du moteur Tomb Raider : %d, carte : \"%s\"";
@@ -56,11 +56,11 @@ sys_notify[1006]    = "Nombre de pièces : %d";
 sys_notify[1007]    = "Nombre de textures : %d";
 sys_notify[1008]    = "Espacement de ligne de la console : %f";
 sys_notify[1009]    = "Nombre de lignes de la console: %d";
-sys_notify[1010]    = "TRIGGER: timer - %d, mask - %02X";
-sys_notify[1011]    = "Activate object %d by %d";
-sys_notify[1012]    = "Loaded fader picture: %s";
-sys_notify[1013]    = "Trigger array cleaned up successfully!";
-sys_notify[1014]    = "Entity function array cleaned up successfully!";
+sys_notify[1010]    = "DÉCLENCHEUR: timer - %d, masque - %02X";
+sys_notify[1011]    = "Objet %d activé par %d";
+sys_notify[1012]    = "Image de transition chargée : %s";
+sys_notify[1013]    = "Liste de déclencheurs effacée avec succès !";
+sys_notify[1014]    = "Liste de fonctions d'éntités effacée avec succès !";
 sys_notify[1015]    = "Fichier de niveau PC chargé (chargeur VT utilisé).";
 sys_notify[1016]    = "Fichier de niveau PSX chargé.";
 sys_notify[1017]    = "Fichier de niveau Dreamcast chargé.";
